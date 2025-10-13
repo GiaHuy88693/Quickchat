6c2a821fc69ccf2c07b0596c37a64a32ae98439c
GiaHuy88693 <giahuyvovinam@gmail.com>
2025-10-13 22:23:20 +0700

    feat(auth): completed call api login and logout

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,21 @@
-import React from 'react'
-import { Route, Routes } from 'react-router-dom'
-import HomePage from './pages/HomePage'
-import LoginPage from './pages/LoginPage'
-import ProfilePage from './pages/ProfilePage'
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import LoginPage from "./pages/LoginPage";
+import ProfilePage from "./pages/ProfilePage";
+import { Toaster } from "react-hot-toast";
 
 const App = () => {
   return (
     <div className="bg-[url('./src/assets/bgImage.svg')] bg-contain">
+      <Toaster />
       <Routes>
-        <Route path='/' element={<HomePage/>}/>
-        <Route path='/login' element={<LoginPage/>}/>
-        <Route path='/Profile' element={<ProfilePage/>}/>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/Profile" element={<ProfilePage />} />
       </Routes>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
